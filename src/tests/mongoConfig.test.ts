abd327a2e2eb2fb2dc424c8878234d90fe2af898
Adrisson Samersla <adrissonsamersla@gmail.com>
2019-12-01 16:57:42 -0300

    Disconnecting to MongoAtlas after the tests

--- src/tests/mongoConfig.test.ts
+++ src/tests/mongoConfig.test.ts
@@ -1,6 +1,10 @@
 import { mongoConnector, mongoDisconnector } from '../config/MongoConfig';
 
 describe('MongoDB Configurations', () => {
+    afterAll(async () => {
+        await mongoDisconnector();
+    });
+
     it('Testing connection and disconnection', async () => {
         await mongoConnector();
         await mongoDisconnector();
--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,7 +1,22 @@
 import req from 'supertest';
 import server from './../server';
 
+import { mongoDisconnector } from './../config/MongoConfig';
+
 test('[GET] /', async () => {
     const res = await req(server).get('/');
     expect(res.text).toBe("Don't Panic! All right!");
+
+    await mongoDisconnector();
+});
+
+describe('Server Integration', () => {
+    afterAll(async () => {
+        await mongoDisconnector();
+    });
+
+    it('[GET] /', async () => {
+        const res = await req(server).get('/');
+        expect(res.text).toBe("Don't Panic! All right!");
+    });
 });
