d2fb6567e965e1cfa9133bc0020033d69a801615
Adrisson Samersla <adrissonsamersla@gmail.com>
2019-11-30 23:49:49 -0300

    some tests didn't exited because of some ongoing process, which caused some CI failures

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,7 +1,17 @@
 import req from 'supertest';
+
+import { mongoDisconnector } from '../config/MongoConfig';
+
 import server from './../server';
 
-test('[GET] /', async () => {
-    const res = await req(server).get('/');
-    expect(res.text).toBe("Don't Panic! All right!");
+describe('Testing the server', () => {
+    afterAll(async () => {
+        await mongoDisconnector();
+    });
+
+    it('[GET] /', async done => {
+        const res = await req(server).get('/');
+        expect(res.text).toBe("Don't Panic! All right!");
+        done();
+    });
 });
