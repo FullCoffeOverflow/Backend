5c84c098f0e7f7587002f79e42303b32c02bdf77
Adrisson Samersla <adrissonsamersla@gmail.com>
2019-12-01 17:06:32 -0300

    Forgot to erase some lines

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -3,13 +3,6 @@ import server from './../server';
 
 import { mongoDisconnector } from './../config/MongoConfig';
 
-test('[GET] /', async () => {
-    const res = await req(server).get('/');
-    expect(res.text).toBe("Don't Panic! All right!");
-
-    await mongoDisconnector();
-});
-
 describe('Server Integration', () => {
     afterAll(async () => {
         await mongoDisconnector();
